e16130f04fe578bc0e319f2a26b4a03299d7534f
Rana Khalil <rana.ae.khalil@gmail.com>
2017-06-19 19:19:57 -0700

    PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <math.h>
 
 using namespace std;
 
@@ -12,18 +13,27 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-	Kp = Kp;
-	Ki = Ki;
-	Kd = Kd;
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
 	d_error = cte - p_error;
 	p_error = cte;
-	i_error += cte;
+	i_error += cte;	
 }
 
 double PID::TotalError() {
-	return p_error + i_error + d_error;
+	double steer = -Kp * p_error - Kd * d_error - Ki * i_error;
+	if (fabs(steer) > 1.0)
+	{
+		steer *= 0.5;
+	}
+	else
+	{
+		steer *= -0.5;
+	}
+	return steer;
 }
 
